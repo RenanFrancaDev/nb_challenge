7556bae85904977d02f9a8e488342abed52ad032
Renan Franca <renanfranca.dev@gmail.com>
2024-05-02 22:25:03 -0300

    feat: componente para banner na home criado

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,10 +1,14 @@
 import Navbar from "@/app/components/Navbar";
+import PromoBanner from "./components/promo-banner";
 
 const Home = () => {
   return (
-    <nav>
-      <Navbar />
-    </nav>
+    <section>
+      <PromoBanner
+        src="/banner_home/banner_home01.svg"
+        alt="around 10% of discount"
+      />
+    </section>
   );
 };
 
