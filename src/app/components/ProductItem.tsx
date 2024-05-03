8009b16194d759fdb46de8e6d9862988fb5c766a
Renan Franca <renanfranca.dev@gmail.com>
2024-05-03 12:49:59 -0300

    chore: rating adicionado com sucesso

--- src/app/components/ProductItem.tsx
+++ src/app/components/ProductItem.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import { IProducts } from "../interfaces/product";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
 
 const ProductItem = ({ product }: { product: IProducts }) => {
+  const computeRating = (rating: number) => {
+    const widthrate = (rating * 110) / 5;
+    return widthrate;
+  };
+
   return (
     <Link href={`/product/${product.id}`}>
-      <div className="flex h-[300px] w-[160px] flex-col gap-2 rounded-md border hover:cursor-pointer">
-        <div className="flex h-[200px] w-full items-center justify-center bg-white">
+      <div className="flex h-[320px] w-[160px] flex-col gap-2 rounded-md border hover:cursor-pointer">
+        <div className="flex h-[180px] w-full items-center justify-center bg-white">
           <Image
             src={product.image}
             height={100}
@@ -20,7 +27,7 @@ const ProductItem = ({ product }: { product: IProducts }) => {
           />
         </div>
 
-        <div className="flex h-[100px] flex-col justify-between p-1">
+        <div className="flex h-[140px] flex-col justify-between p-1">
           <p className="block overflow-hidden text-ellipsis text-sm">
             {product.title.length > 50
               ? product.title.slice(0, 32) + "..."
@@ -30,6 +37,22 @@ const ProductItem = ({ product }: { product: IProducts }) => {
           <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
             R$ {Number(product.price).toFixed(2).replace(".", ",")}
           </p>
+          <div className="flex flex-col">
+            <Box
+              sx={{
+                "& > legend": { mt: 2 },
+              }}
+            >
+              <Rating
+                name="read-only"
+                precision={0.1}
+                value={product.rating.rate}
+                readOnly
+              />
+            </Box>
+
+            <small>{product.rating.count} avaliações</small>
+          </div>
         </div>
       </div>
     </Link>
