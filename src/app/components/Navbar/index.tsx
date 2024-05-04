96e7dd1541d5e345cd4bcc263ddfaf2e736136fb
Renan Franca <renanfranca.dev@gmail.com>
2024-05-04 12:11:07 -0300

    fix: alteração de configuração apra adaptar o navbar fixed

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-[1.875rem]">
+    <nav
+      className="sticky top-0 z-50 flex items-center justify-between border-b-[1px] border-primary bg-black p-[1.875rem]"
+      style={{ position: "fixed", top: 0, left: 0, width: "100%" }}
+    >
       <Sidebar />
       <Link href="/">
         <h1 className="text-lg font-bold">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import CartProvider from "./providers/cart";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex h-full flex-col pt-[100px]`}>
         <CartProvider>
           <Navbar />
-          {children}
+          <main className=" flex-1">{children}</main>
+          <Footer />
         </CartProvider>
       </body>
     </html>
