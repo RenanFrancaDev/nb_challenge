4abf85d8ef89e58f7fe2ac489f7783c049bf428b
Renan Franca <renanfranca.dev@gmail.com>
2024-05-04 10:59:40 -0300

    feat: configuração de responsivo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "390px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       backgroundImage: {
         "category-item-gradient":
