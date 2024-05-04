0a5065b964331b03c7a028cd9d1f979f293946bb
Renan Franca <renanfranca.dev@gmail.com>
2024-05-04 12:11:57 -0300

    feat: criação do footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent w-full px-8 py-4 text-center text-sm opacity-75">
+      © 2023 Copyright{" "}
+      <span className="font-semibold text-primary">Null Bug Store</span>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
